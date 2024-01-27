0f841c99a350a7ab05cdcf8edb35ccb102660847
vidaibao <vutrungtin@gmail.com>
2024-01-27 22:23:06 +0900

    data grid view search

--- StudentListForm.Designer.cs
+++ StudentListForm.Designer.cs
@@ -37,9 +37,27 @@
             pnlImage = new Panel();
             dgvStudentList = new DataGridView();
             btnLoadData = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            txtID = new TextBox();
+            txtName = new TextBox();
+            txtAddress = new TextBox();
+            txtYob = new TextBox();
+            txtGpa = new TextBox();
+            groupBox1 = new GroupBox();
+            button2 = new Button();
+            btnAddNew = new Button();
+            btnSearch = new Button();
+            dgvSearchResult = new DataGridView();
+            txtSearch = new TextBox();
             ((System.ComponentModel.ISupportInitialize)picAvatar).BeginInit();
             pnlImage.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)dgvStudentList).BeginInit();
+            groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvSearchResult).BeginInit();
             SuspendLayout();
             // 
             // button1
@@ -79,10 +97,9 @@
             // picAvatar
             // 
             picAvatar.BorderStyle = BorderStyle.Fixed3D;
-            picAvatar.Location = new Point(0, 3);
+            picAvatar.Location = new Point(0, 11);
             picAvatar.Name = "picAvatar";
-            picAvatar.Size = new Size(378, 280);
-            picAvatar.SizeMode = PictureBoxSizeMode.AutoSize;
+            picAvatar.Size = new Size(297, 229);
             picAvatar.TabIndex = 2;
             picAvatar.TabStop = false;
             // 
@@ -99,36 +116,198 @@
             // 
             pnlImage.AutoScroll = true;
             pnlImage.Controls.Add(picAvatar);
-            pnlImage.Location = new Point(29, 115);
+            pnlImage.Location = new Point(29, 113);
             pnlImage.Name = "pnlImage";
-            pnlImage.Size = new Size(393, 297);
+            pnlImage.Size = new Size(300, 243);
             pnlImage.TabIndex = 4;
             // 
             // dgvStudentList
             // 
+            dgvStudentList.AllowUserToResizeColumns = false;
+            dgvStudentList.AllowUserToResizeRows = false;
+            dgvStudentList.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.ColumnHeader;
             dgvStudentList.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgvStudentList.Location = new Point(428, 115);
+            dgvStudentList.EnableHeadersVisualStyles = false;
+            dgvStudentList.Location = new Point(348, 175);
             dgvStudentList.Name = "dgvStudentList";
             dgvStudentList.RowTemplate.Height = 25;
-            dgvStudentList.Size = new Size(395, 297);
+            dgvStudentList.Size = new Size(399, 240);
             dgvStudentList.TabIndex = 5;
+            dgvStudentList.SelectionChanged += ShowAStudent;
             // 
             // btnLoadData
             // 
-            btnLoadData.Location = new Point(428, 16);
+            btnLoadData.Location = new Point(422, 15);
             btnLoadData.Name = "btnLoadData";
-            btnLoadData.Size = new Size(102, 29);
+            btnLoadData.Size = new Size(109, 29);
             btnLoadData.TabIndex = 6;
             btnLoadData.Text = "Load Data";
             btnLoadData.UseVisualStyleBackColor = true;
             btnLoadData.Click += LoadData;
             // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(9, 27);
+            label1.Name = "label1";
+            label1.Size = new Size(18, 15);
+            label1.TabIndex = 7;
+            label1.Text = "ID";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(9, 56);
+            label2.Name = "label2";
+            label2.Size = new Size(38, 15);
+            label2.TabIndex = 7;
+            label2.Text = "Name";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(9, 90);
+            label3.Name = "label3";
+            label3.Size = new Size(49, 15);
+            label3.TabIndex = 7;
+            label3.Text = "Address";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(216, 28);
+            label4.Name = "label4";
+            label4.Size = new Size(27, 15);
+            label4.TabIndex = 7;
+            label4.Text = "Yob";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(216, 53);
+            label5.Name = "label5";
+            label5.Size = new Size(28, 15);
+            label5.TabIndex = 7;
+            label5.Text = "Gpa";
+            // 
+            // txtID
+            // 
+            txtID.Location = new Point(74, 24);
+            txtID.Name = "txtID";
+            txtID.Size = new Size(100, 23);
+            txtID.TabIndex = 8;
+            // 
+            // txtName
+            // 
+            txtName.Location = new Point(74, 53);
+            txtName.Name = "txtName";
+            txtName.Size = new Size(100, 23);
+            txtName.TabIndex = 8;
+            // 
+            // txtAddress
+            // 
+            txtAddress.Location = new Point(74, 82);
+            txtAddress.Name = "txtAddress";
+            txtAddress.Size = new Size(100, 23);
+            txtAddress.TabIndex = 8;
+            // 
+            // txtYob
+            // 
+            txtYob.Location = new Point(278, 22);
+            txtYob.Name = "txtYob";
+            txtYob.Size = new Size(100, 23);
+            txtYob.TabIndex = 8;
+            // 
+            // txtGpa
+            // 
+            txtGpa.Location = new Point(278, 48);
+            txtGpa.Name = "txtGpa";
+            txtGpa.Size = new Size(100, 23);
+            txtGpa.TabIndex = 8;
+            // 
+            // groupBox1
+            // 
+            groupBox1.BackColor = SystemColors.ControlLight;
+            groupBox1.Controls.Add(txtGpa);
+            groupBox1.Controls.Add(txtYob);
+            groupBox1.Controls.Add(txtAddress);
+            groupBox1.Controls.Add(txtName);
+            groupBox1.Controls.Add(txtID);
+            groupBox1.Controls.Add(label5);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Location = new Point(348, 50);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(399, 118);
+            groupBox1.TabIndex = 9;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Student Info";
+            // 
+            // button2
+            // 
+            button2.Location = new Point(422, 15);
+            button2.Name = "button2";
+            button2.Size = new Size(109, 29);
+            button2.TabIndex = 6;
+            button2.Text = "Load Data";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += LoadData;
+            // 
+            // btnAddNew
+            // 
+            btnAddNew.Location = new Point(562, 15);
+            btnAddNew.Name = "btnAddNew";
+            btnAddNew.Size = new Size(109, 29);
+            btnAddNew.TabIndex = 6;
+            btnAddNew.Text = "Add New";
+            btnAddNew.UseVisualStyleBackColor = true;
+            btnAddNew.Click += AddNewStudent;
+            // 
+            // btnSearch
+            // 
+            btnSearch.Location = new Point(764, 132);
+            btnSearch.Name = "btnSearch";
+            btnSearch.Size = new Size(109, 29);
+            btnSearch.TabIndex = 6;
+            btnSearch.Text = "Search";
+            btnSearch.UseVisualStyleBackColor = true;
+            btnSearch.Click += SearchStudents;
+            // 
+            // dgvSearchResult
+            // 
+            dgvSearchResult.AllowUserToResizeColumns = false;
+            dgvSearchResult.AllowUserToResizeRows = false;
+            dgvSearchResult.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.ColumnHeader;
+            dgvSearchResult.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvSearchResult.EnableHeadersVisualStyles = false;
+            dgvSearchResult.Location = new Point(753, 175);
+            dgvSearchResult.Name = "dgvSearchResult";
+            dgvSearchResult.RowTemplate.Height = 25;
+            dgvSearchResult.Size = new Size(377, 240);
+            dgvSearchResult.TabIndex = 5;
+            dgvSearchResult.SelectionChanged += ShowAStudent;
+            // 
+            // txtSearch
+            // 
+            txtSearch.Location = new Point(898, 135);
+            txtSearch.Name = "txtSearch";
+            txtSearch.Size = new Size(143, 23);
+            txtSearch.TabIndex = 8;
+            // 
             // StudentListForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(835, 424);
+            ClientSize = new Size(1150, 424);
+            Controls.Add(groupBox1);
+            Controls.Add(btnSearch);
+            Controls.Add(btnAddNew);
+            Controls.Add(button2);
+            Controls.Add(txtSearch);
             Controls.Add(btnLoadData);
+            Controls.Add(dgvSearchResult);
             Controls.Add(dgvStudentList);
             Controls.Add(pnlImage);
             Controls.Add(lblFileName);
@@ -141,8 +320,10 @@
             Click += SayGoodBye;
             ((System.ComponentModel.ISupportInitialize)picAvatar).EndInit();
             pnlImage.ResumeLayout(false);
-            pnlImage.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)dgvStudentList).EndInit();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvSearchResult).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -158,5 +339,21 @@
         private Panel pnlImage;
         private DataGridView dgvStudentList;
         private Button btnLoadData;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private TextBox txtID;
+        private TextBox txtName;
+        private TextBox txtAddress;
+        private TextBox txtYob;
+        private TextBox txtGpa;
+        private GroupBox groupBox1;
+        private Button button2;
+        private Button btnAddNew;
+        private Button btnSearch;
+        private DataGridView dgvSearchResult;
+        private TextBox txtSearch;
     }
 }
--- StudentListForm.cs
+++ StudentListForm.cs
@@ -1,12 +1,46 @@
 ﻿using Giaolang.LINQ.V3.StudentMgt;
+using System.Windows.Forms;
 
 namespace StudentManagement
 {
     public partial class StudentListForm : Form
     {
+        List<Student> _ds = new List<Student>();
+
         public StudentListForm()
         {
             InitializeComponent();
+
+            // Call the method to customize the header
+            CustomizeDataGridViewHeader();
+
+            // Initialize data
+            InitData();
+        }
+
+        private void InitData()
+        {
+            _ds.Add(new Student() { Id = "SE1", Name = "An", Address = "Dĩ An", Gpa = 8.8, Yob = 2003 });
+            _ds.Add(new Student() { Id = "SE2", Name = "Bình", Address = "Bình Dương", Gpa = 9.0, Yob = 2008 });
+            _ds.Add(new Student() { Id = "SE5", Name = "Dương", Address = "Tân Bình", Gpa = 5.0, Yob = 2005 });
+            _ds.Add(new Student() { Id = "SE4", Name = "Dũng", Address = "Châu Thành", Gpa = 5.0, Yob = 2006 });
+            _ds.Add(new Student() { Id = "SE3", Name = "Thành", Address = "Long An", Gpa = 8.0, Yob = 2000 });
+            _ds.Add(new Student() { Id = "SE6", Name = "Thinh", Address = "Rach Gia", Gpa = 8.2, Yob = 2001 });
+            _ds.Add(new Student() { Id = "SE8", Name = "Xuan", Address = "Long An", Gpa = 8.3, Yob = 2003 });
+            _ds.Add(new Student() { Id = "SE7", Name = "Trinh", Address = "Bến tre", Gpa = 7.9, Yob = 2002 });
+        }
+
+        private void CustomizeDataGridViewHeader()
+        {
+            // Set the header style
+            /* Set the EnableHeadersVisualStyles flag for the data grid view to False, 
+             * and set the background colour via the ColumnHeadersDefaultCellStyle.BackColor property*/
+            dgvStudentList.EnableHeadersVisualStyles = false;
+            dgvStudentList.ColumnHeadersDefaultCellStyle.BackColor = Color.Aqua; // Optional: Change the background color of the header
+            dgvStudentList.ColumnHeadersDefaultCellStyle.Font = new Font(dgvStudentList.Font, FontStyle.Bold); // Make the font bold
+            dgvStudentList.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter; // Optional: Align text to center
+                                                                                                                // 
+
         }
 
         private void SayHello(object sender, EventArgs e)
@@ -43,19 +77,66 @@ namespace StudentManagement
 
         private void LoadData(object sender, EventArgs e)
         {
-            List<Student> ds = new List<Student>()
-            {
-                new Student() {Id = "SE1", Name = "An", Address = "Dĩ An", Gpa = 8.8, Yob = 2003},
-                new Student() {Id = "SE2", Name = "Bình", Address = "Bình Dương", Gpa = 9.0, Yob = 2008},
-                new Student() {Id = "SE5", Name = "Dương", Address = "Tân Bình", Gpa = 5.0, Yob = 2005},
-                new Student() {Id = "SE4", Name = "Dũng", Address = "Châu Thành", Gpa = 5.0, Yob = 2006},
-                new Student() {Id = "SE3", Name = "Thành", Address = "Long An", Gpa = 8.0, Yob = 2000},
-                new Student() {Id = "SE6", Name = "Thinh", Address = "Rach Gia", Gpa = 8.2, Yob = 2001},
-                new Student() {Id = "SE8", Name = "Xuan", Address = "Long An", Gpa = 8.3, Yob = 2003},
-                new Student() {Id = "SE7", Name = "Trinh", Address = "Ben tre", Gpa = 7.9, Yob = 2002},
-            };
+            dgvStudentList.DataSource = _ds;    //1983  andr
+            //const int WIDTH = 70;
+            //dgvStudentList.Columns[0].Width = WIDTH;
+            //dgvStudentList.Columns[1].Width = WIDTH;
+            //dgvStudentList.Columns[2].Width = 100;
+            //dgvStudentList.Columns[3].Width = WIDTH;
+            //dgvStudentList.Columns[4].Width = WIDTH;
+        }
 
-            dgvStudentList.DataSource = ds;
+        private void ShowAStudent(object sender, EventArgs e)
+        {
+            if (dgvStudentList.SelectedRows.Count > 0)
+            {
+                DataGridViewRow selectedRow = dgvStudentList.SelectedRows[0]; // 1st row in selected rows
+                txtID.Text = selectedRow.Cells["ID"].Value.ToString();
+                txtName.Text = selectedRow.Cells["Name"].Value.ToString();
+                txtAddress.Text = selectedRow.Cells["Address"].Value.ToString();
+                txtYob.Text = selectedRow.Cells[3].Value.ToString();
+                txtGpa.Text = selectedRow.Cells[4].Value.ToString();
+            }
+        }
+
+        private void AddNewStudent(object sender, EventArgs e)
+        {
+            // create new Student
+            if (_ds.Where(x => x.Id.Equals(txtID.Text)).Select(x => x.Id).ToArray().Length > 0)
+            {
+                MessageBox.Show("ID is already exits!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                return;
+            }
+            // add to _ds
+            _ds.Add(new Student()
+            {
+                Id = txtID.Text,
+                Name = txtName.Text,
+                Address = txtAddress.Text,
+                Gpa = double.Parse(txtGpa.Text),
+                Yob = int.Parse(txtYob.Text)
+            });
+
+            // clear grid
+            dgvStudentList.DataSource = null;
+
+            // view grid again
+            dgvStudentList.DataSource = _ds;
+        }
+
+        private void SearchStudents(object sender, EventArgs e)
+        {
+            var result = _ds.Where(x => x.Name.ToLower().Contains(txtSearch.Text.ToLower()) 
+                                    || x.Address.ToLower().Contains(txtSearch.Text.ToLower())).ToList();
+            if (result.Count > 0)
+            {
+                dgvSearchResult.DataSource = result;
+            }
+            else
+            {
+                MessageBox.Show("No student found!");
+            }
+
         }
     }
 }
